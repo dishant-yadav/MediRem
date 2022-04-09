ee34abe4ce7108a77ee8254b8105a92ba988394e
“[Dishant <[dishantyadav599@gmail.com]>
2022-04-09 19:59:27 +0530

    added the show medicine page

--- static/scripts/showMed.js
+++ static/scripts/showMed.js
@@ -15,12 +15,3 @@ tom.addEventListener("click", function () {
   tomList.style.display = "block";
   todList.style.display = "none";
 });
-// let noOfMed = document.getElementsByClassName("f");
-// if (noOfMed.length > 3) {
-//     noOfMed.forEach(element => {
-//         noOfMed[2].style.display = "none";
-//     });
-//   prev.style.display = "block";
-//   next.style.display = "block";
-// }
-// noOfMed[2].getElementsByClassName.display = "none";
